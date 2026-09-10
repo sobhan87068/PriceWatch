167b5446d2e9cea1dfcc3de52fc6c8da1642448e
sobhan <sobhan.aryannamazi@gmail.com>
2026-09-10 23:02:09 +0330

    Initial commit

---
+++ src/test/java/ir/sban/spring/pricewatch/PriceWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sban.spring.pricewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
